eac85eafd10df0ba7aacf1b7aaa59653dc9db19e
Moses <mosesamwoma@gmail.com>
2025-12-08 18:03:59 +0300

    updated

--- tests/test_preprocessing.py
+++
@@ -1,6 +0,0 @@
-from src.preprocessing import clean_text
-
-def test_clean_text_basic():
-    text = "Hello WORLD! Visit http://example.com"
-    cleaned = clean_text(text)
-    assert cleaned == "hello world visit"
