6ffed0c3eaedc7cd2f99f31c56de76a8a7bd3771
Moses <mosesamwoma@gmail.com>
2025-12-30 18:26:37 +0300

    refactor: streamline CORS middleware setup and update prediction response format

--- api/app.py
+++ api/app.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field
 import joblib
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Spam Email Detection API",
     description="API to classify email text as spam or not spam(ham)",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,14 +33,11 @@ def home():
     }
 
 class EmailText(BaseModel):
-    text: str = Field(
-        ...,
-        example="You've won a free iPhone! Click to claim."
-    )
+    text: str = Field(..., example="You've won a free iPhone! Click to claim.")
 
 @app.post("/predict")
 def predict(email: EmailText):
     X = vectorizer.transform([email.text])
     pred = model.predict(X)[0]
-    result = "spam" if pred == 1 else "not spam"
-    return {"prediction": result}
+    spam_bool = bool(pred)
+    return {"spam": spam_bool}
